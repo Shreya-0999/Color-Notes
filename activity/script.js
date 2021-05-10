66e6e48837267eeac8992bb8868dde05f615bdf2
Shreya <shreya21999@gmail.com>
2021-05-10 12:21:11 +0530

    first commit

--- activity/script.js
+++ activity/script.js
@@ -19,7 +19,6 @@ if (localStorage.getItem("allTask")) {
         createTask(color, task, false, uid); // false to tell that its from local storage
     }
 }
-console.log(taskArr);
 
 //---------------Navigation Functionality-----------------
 plusBtn.addEventListener("click", createModal);
@@ -74,15 +73,19 @@ function changeFilter(displayColor) {
 }
 
 //---------------Creating Modal for adding new task-----------------
-function createModal() {
+function createModal(task, flag, color, uid, expandBtn) {
+    if (typeof (task) == "object") {   // done for expanding feature to check that whether it is called from expand button or plusbtn (if plusbtn then it will have object inside it)
+        task = "";
+    }
     let modal_container = document.querySelector(".modal_container");
     if (modal_container == null) {       // to check whether the container is already present of not
-        plusBtn.classList.add("active");  // change its backgroung to tell its active
+        if (!flag)   // to check whether it is called from expand btn or plus btn
+            plusBtn.classList.add("active");  // change its backgroung to tell its active
         modal_container = document.createElement("div");
         modal_container.setAttribute("class", "modal_container");     //setting its class = modal_container
         //adding html of the conntainer
         modal_container.innerHTML = `<div class="input_container">    
-                <textarea class="modal_input" placeholder = "Enter your task"></textarea>
+                <textarea class="modal_input" placeholder = "Enter your task">${task}</textarea>
             </div>
             <div class="modal_filter"> 
                 <div class="pink filter"></div>
@@ -91,15 +94,16 @@ function createModal() {
                 <div class="black filter"></div>
             </div>`
         body.appendChild(modal_container);   //attaching it to the body (whenever plusbtn will be clicked this modal will be attached to the body)
-        handleModal(modal_container);     // for handling the actions inside the modal container
+        if (flag)
+            expandModal(modal_container, color, uid, expandBtn);
+        else
+            handleModal(modal_container);     // for handling the actions inside the modal container
     }
     else {
         modal_container.remove();
         plusBtn.classList.remove("active");   // to remove the active feature
         main_container.style.opacity = 1;
     }
-    // let input = modal_container.querySelector(".modal_input");
-    // input.value = "";   // if the conatiner is already present then clear its value
 }
 
 function handleModal(modal_container) {
@@ -112,7 +116,6 @@ function handleModal(modal_container) {
             // removing the border class from all of the filter color available
             modal_filters.forEach((filter) => {
                 filter.classList.remove("border");
-                console.log(filter);
             })
             // adding the border class to the filter color clicked
             modal_filters[i].classList.add("border");
@@ -127,6 +130,39 @@ function handleModal(modal_container) {
             modal_container.remove();         // removing the modal container
             main_container.style.opacity = 1;
             createTask(current_color, input_area.value, true);   // creating the task on the body with the color and the task entered(true : to tell its entered from the UI not local storage)
+        }
+    })
+}
+
+// if the task container is expanded
+function expandModal(modal_container, color, uid, expandBtn) {
+    main_container.style.opacity = 0.5;
+    let currColor = color.classList[1];  // getting the color of the task container
+    // selecting same color which is on task container with the color filter from the modal container
+    let modal_filters = document.querySelectorAll(".modal_filter .filter");
+    modal_filters.forEach((filter) => {
+        filter.classList.remove("border");
+    })
+    // as the task container color is met adding the border to it
+    modal_filters.forEach((filter) => {
+        if (filter.classList[0] == currColor)
+            filter.classList.add("border");
+    })
+
+    // if any changes is done in the input container then reflecting it back in the task container
+    let task_cont = '';
+    let uidArr = document.querySelectorAll(".uid");  // collecting the all the uid present in the document
+    for (let i = 0; i < uidArr.length; i++)   // matching all the uids with the one that has been selected 
+        if (uid == uidArr[i])
+            task_cont = uidArr[i].parentNode.parentNode.parentNode;   // storing the task_container of the matched uid to reflect the changes in the task_area
+    let input_area = modal_container.querySelector(".modal_input");   // as the enter key is pressed in the modal container
+    input_area.addEventListener("keydown", function (e) {
+        if (e.key == "Enter" && input_area != null) {
+            modal_container.remove();                                 // it will remove the modal container
+            let t_a = task_cont.querySelector(".task_area");          // get the task_area of the container
+            t_a.innerText = input_area.value;                         // and reflect the changes done that has been made to the modal
+            expandBtn.classList.remove("task_icon_active");
+            main_container.style.opacity = 1;
         }
     })
 }
@@ -163,42 +199,57 @@ function createTask(color, task, flag, id) {
     let color_bar = task_container.querySelector(".color_bar");
     color_bar.addEventListener("click", changeColor);
 
-    let task_area = task_container.querySelector(".task_area");
-    // getting the edit option available in the task container
-    let task_iconArr = task_container.querySelectorAll(".task_icon_container");
+    let task_iconArr = task_container.querySelectorAll(".task_icon_container");  // getting the edit option available in the task container
+
     // On clicking the pencil icon the content will become editable
     let pencilBtn = task_iconArr[0];
-    pencilBtn.addEventListener("click", function(){
-        let isActive = pencilBtn.classList.contains("task_icon_active");
-        if(isActive){
-            pencilBtn.classList.remove("task_icon_active");
-            task_area.setAttribute("contenteditable", "false")
-        }
-        else{
-            pencilBtn.classList.add("task_icon_active");
-            task_area.setAttribute("contenteditable", "true")
-        }
-        // console.log(pencilBtn)
-    })
+    pencilBtn.addEventListener("click", pencilFunction)
 
     let expandBtn = task_iconArr[1];
-    expandBtn.addEventListener("click", function(){
-        let isActive = expandBtn.classList.contains("task_icon_active");
-        if(isActive){
-            expandBtn.classList.remove("task_icon_active");
-        }
-        else{
-            expandBtn.classList.add("task_icon_active");
-        }
-    })
+    expandBtn.addEventListener("click", expandFunction);
 
     // if their is any editing done on the task then it must be reflected back into the local storage
+    let task_area = task_container.querySelector(".task_area");
     task_area.addEventListener("keypress", editTask);
 
     // to delete any task if the delete state  is true
     task_container.addEventListener("click", deleteTask);
 }
 
+//adding pencil feature
+function pencilFunction(e) {
+    let pencilBtn = e.currentTarget;
+    let task_area = pencilBtn.parentNode.parentNode.children[1];
+    let isActive = pencilBtn.classList.contains("task_icon_active");
+    if (isActive) {      // if button active then close
+        pencilBtn.classList.remove("task_icon_active");
+        task_area.setAttribute("contenteditable", "false")
+    }
+    else {    // if button not active then make it active and perfrom the task
+        pencilBtn.classList.add("task_icon_active");
+        task_area.setAttribute("contenteditable", "true")
+    }
+}
+
+//adding the expanding feature
+function expandFunction(e) {
+    let expandBtn = e.currentTarget;
+    let task_area = expandBtn.parentNode.parentNode.children[1];   // getting the task area of the container selected
+    let isActive = expandBtn.classList.contains("task_icon_active");  // adding active feature
+    if (isActive) {
+        expandBtn.classList.remove("task_icon_active");
+        createModal();
+    }
+    else {
+        expandBtn.classList.add("task_icon_active");
+        let task = task_area.innerText;    // extracting the text inside the container to display it in modal
+        let color = expandBtn.parentNode.parentNode.parentNode.children[0];  // extracting colo and uid for the task container selected
+        let uid = expandBtn.parentNode.children[0];
+        createModal(task, true, color, uid, expandBtn);   // creating the expanded version
+    }
+}
+
+//changing the color of the color bar of task container
 function changeColor(e) {
     let colorArr = ["pink", "blue", "green", "black"];
     let color_bar = e.currentTarget;
@@ -222,6 +273,7 @@ function changeColor(e) {
 
 }
 
+//if their is any change in the task then update it on local storage
 function editTask(e) {
     let task_area = e.currentTarget;
     let id = task_area.parentNode.children[0].innerText;          // to know the uid of the task clicked
@@ -237,6 +289,8 @@ function editTask(e) {
     }
 }
 //---------------Deleting Task-----------------
+
+//checking the state of cross button i.e whether active or not
 function setDeleteState(e) {
     let crossBtn = e.currentTarget;
     if (deleteState == false) {
@@ -248,6 +302,7 @@ function setDeleteState(e) {
     deleteState = !deleteState;    // to change the state of cross btn i.e to know whether the btn is on of off
 }
 
+//deleting the task if cross btn is active and updating it on local
 function deleteTask(e) {
     if (deleteState == true) {  // if the cross button is on then perform the operation
         let task_container = e.currentTarget;
